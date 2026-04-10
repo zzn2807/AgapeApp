2e27fd5dd156fd1268c631185d6412932df23b03
David Nwokolo <nwokolodz@gmail.com>
2026-04-10 01:29:59 -0400

    Added Misc and Footer data

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -6,9 +6,9 @@ function Footer() {
 
     return (
         <div className="footer">
-            <Facebook className="icon"/>
-            <Email className="icon"/>
-            <Twitter className="icon"/>
+            <a href="https://www.facebook.com/profile.php?id=100054652800504"><Facebook className="icon"/></a>
+            <a href="mailto:hr@hopehealthsystems.com"><Email className="icon"/></a>
+            <a href="https://x.com/hopehealth_inc"><Twitter className="icon"/></a>
         </div>
 
     );
--- src/Components/Misc.js
+++ src/Components/Misc.js
@@ -1,22 +1,44 @@
 import phInfoImg from "../img/ph-info.jpg"
 import "../css/Page.css"
+import esmh101 from "../files/ESMH101.pdf"
+import esmhflyer from "../files/ESMH Flyer.pdf"
+import esmhQR from "../files/ESMH QR code Flyer.pdf"
+import dinnerFlyer from "../files/Family enagagment dinner flyer.pdf"
+import hhs_bss from "../files/HHS Behavioral Support Services.pdf"
 function Misc() {
+    const handleOpenPdf = (documentPath, filename) => {
+        if (window.cordova) {
+            window.resolveLocalFileSystemURL(documentPath, function (fileEntry) {
+                window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function (dirEntry) {
+                    fileEntry.copyTo(dirEntry, filename, function (newFileEntry) {
+                        cordova.plugins.fileOpener2.open(newFileEntry.nativeURL, 'application/pdf',
+                            {
+                                error: function (e) {
+                                    console.log('Error status: ' + e.status + ' - Error message: ' + e.message);
+                                },
+                                success: function () {
+                                    console.log('file opened successfully');
+                                }
+                            }
+                        );
+                    });
+                });
+            });
+
+
+        }
+    }
     return (
         <div className="content">
             <h1>Public Health Info</h1>
-            <ul className="content-ul">
-                <li>Lorem ipsum dolor sit amet, consectetur</li>
-                <li>adipiscing elit. Nam ligula leo, molestie sed est </li>
-                <li>sed, mollis dictum sapien. Donec sit amet ligula </li>
-                <li>consequat, facilisis mi eget, gravida dolor. Nunc t</li>
-                <li>empor leo massa, quis porta nisl accumsan non. </li>
-                <li>Cras eget nisi lacus. Curabitur lectus massa, </li>
-            </ul>
-            <img src={phInfoImg} className="content-img"/>
-            <p>
-                fermentum id nisi a, pulvinar bibendum ipsum. Phasellus et semper neque. Proin tincidunt sollicitudin augue eu bibendum. Sed quis sollicitudin tellus, vel tincidunt tortor. Pellentesque elit nulla, luctus quis sagittis ac, faucibus in justo. Sed et tempor risus. Quisque sem sapien, facilisis a pulvinar at, rutrum et purus.
-            </p>
-            <a className="btn">Click for more info</a>
+            
+            <a href={esmh101} download="ESMH101.pdf" onClick={() => { handleOpenPdf(cordova.file.applicationDirectory + "www/static/media/ESMH101.afd0d2873858e5d20a8f.pdf", "ESMH101.pdf") }} className="btn" id="esmh101">ESMH 101</a>
+            <a href={esmhflyer} download="ESMH Flyer.pdf" onClick={() => { handleOpenPdf(cordova.file.applicationDirectory + "www/static/media/ESMH Flyer.dc1740b2362b6ce1f01c.pdf", "ESMH Flyer.pdf") }} className="btn" id="esmhflyer">ESMH Flyer</a>
+            <a href={esmhQR} download="ESMH QR code Flyer.pdf" onClick={() => { handleOpenPdf(cordova.file.applicationDirectory + "www/static/media/ESMH QR code Flyer.f2ed397c9dbb93c7c378.pdf", "ESMH QR code Flyer.pdf") }} className="btn" id="esmhQR">ESMH QR Codes</a>
+            <a href={hhs_bss} download="HHS Behavioral Support Services.pdf" onClick={() => { handleOpenPdf(cordova.file.applicationDirectory + "www/static/media/HHS Behavioral Support Services.e046e187c6578064d016.pdf", "HHS Behavioral Support Services.pdf") }} className="btn" id="hhs_bss">HHS Behavioral Support Services</a>
+            
+            <h1>Events</h1>
+            <a href={dinnerFlyer} download="Family enagagment dinner flyer.pdf" onClick={() => { handleOpenPdf(cordova.file.applicationDirectory + "www/static/media/Family enagagment dinner flyer.8ff9db53e1f587339399.pdf", "Family enagagment dinner flyer.pdf") }} className="btn" id="dinnerFlyer">ESMH QR Codes</a>
         </div>
     )
 }
